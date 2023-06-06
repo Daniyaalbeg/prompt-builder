8634cb8bddb95eb600b3ef51fd5bf47ba49af57b
Daniyaal Beg <danyaalbeg@gmail.com>
2023-06-07 03:32:03 +0500

    fix sign in and comment out sign out

---
+++ src/app/sign-up/layout.tsx
@@ -0,0 +1,21 @@
+// import { buttonVariants } from "@/components/ui/button";
+// import { cn } from "@/lib/utils";
+// import { Command, Link } from "lucide-react";
+// import { Metadata } from "next";
+
+// type Props = {
+//   children: React.ReactNode;
+// };
+
+// export const metadata: Metadata = {
+//   title: "Prompt Sutff Authentication",
+//   description: "Sign in page for prompt stuff",
+// };
+
+// const SignUpLayout = ({ children }: Props) => {
+// return (
+
+// )
+// }
+
+// export default SignUpLayout;
